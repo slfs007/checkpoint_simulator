2eb722cd5f9bb6a7c5503af88e0fdece5fd4a223
MonkeyKing <mk@localhost.localdomain>
2015-07-30 23:23:06 +0800

    first commit

---
+++ makefile
@@ -0,0 +1,2 @@
+rf_generator:
+	gcc random_file_generator.c -o rf_generator
---
+++ random_file_generator.c
@@ -0,0 +1,13 @@
+
+#include<stdio.h>
+#include<stdlib.h>
+int main ( int argc, char *argv[])
+{
+    
+    if ( argc != 4)
+    {
+        printf("usage: ./rf_generator [file name] [unit size] [unit number]");
+        exit(1);
+    }
+    
+}
