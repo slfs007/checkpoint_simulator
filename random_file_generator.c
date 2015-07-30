93e08b72e06b43165d1d0b424316eaf58479dd86
mkb <slfs009@163.com>
2015-07-30 23:53:46 +0800

    complete the random_file_generator

--- random_file_generator.c
+++ random_file_generator.c
@@ -1,13 +1,42 @@
 
 #include<stdio.h>
 #include<stdlib.h>
+#include<time.h>
+
+void generate_random_file( int unit_num, FILE *file);
+
 int main ( int argc, char *argv[])
 {
+    FILE *rfg_file;
+    int unit_num;
     
-    if ( argc != 4)
+    if ( argc != 3)
     {
-        printf("usage: ./rf_generator [file name] [unit size] [unit number]");
+        perror("usage: ./rf_generator [file name]  [unit number]");
         exit(1);
     }
     
+    if (  NULL == (rfg_file = fopen( argv[1], "w")))
+    {
+        perror("fopen error");
+    }
+    
+    unit_num = atoi(argv[2]);
+    printf("unit num:%d\n",unit_num);
+    
+    generate_random_file(  unit_num,rfg_file);
+    exit(0);
+}
+void generate_random_file(int unit_num, FILE *file)
+{
+    int i;
+    int rand_num;
+    
+    srand(time(NULL));
+    for ( i = 0; i < unit_num;i++)
+    {
+        rand_num = rand();
+        fwrite( &rand_num, sizeof(int), 1, file);
+    }
+    fflush(file);
 }
