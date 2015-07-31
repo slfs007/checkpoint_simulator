d5002ccb510f50256118f7ed92347e6c4e8fe8c6
mkb <slfs009@163.com>
2015-07-31 23:21:20 +0800

    modify db_thread

--- src/checkpoint_simulator/checkpoint_simulator.c
+++ src/checkpoint_simulator/checkpoint_simulator.c
@@ -15,6 +15,8 @@ int main( int argc, char *argv[])
     char *rf_name;
     pthread_t *update_thread_array;
     pthread_t db_thread;
+    int db_thread_arg[2];
+    
     if ( argc != 5)
     {
         perror("usage:./ckp_cimulator [update thread number] [database size] [algorithm type:0-navie 1-copy on update 2-zigzag 3-pingpong] [random file name]");
@@ -23,8 +25,17 @@ int main( int argc, char *argv[])
     db_size = atoi( argv[2]);
     alg_type = atoi( argv[3]);
     rf_name = argv[4];
-    update_thread_array = (pthread_t *)malloc(sizeof(pthread_t) * update_thread_num);
+   
     //start db_thread
+    db_thread_arg[0] = db_size;
+    db_thread_arg[1] = alg_type;
+    if ( 0 != pthread_create( &db_thread, NULL, database_thread, db_thread_arg))
+    {
+        perror("database thread create error!");
+    }
     //start update thread array
+     update_thread_array = (pthread_t *)malloc(sizeof(pthread_t) * update_thread_num);
     //wait for quit
+     pthread_join( db_thread,NULL);
+     exit(1);
 }
--- src/checkpoint_simulator/database.c
+++ src/checkpoint_simulator/database.c
@@ -1,5 +1,16 @@
 #include"database.h"
+#include<stdio.h>
 void *database_thread(void *arg)
 {
+    int db_size = (( int *)arg)[0];
+    int alg_type = (( int *)arg)[1];
     
+    printf("database thread start，db_size:%d alg_type:%d\n",db_size,alg_type);
 }
+int navie_read( int index)
+{
+}
+int navie_write( int index)
+{
+}
+
