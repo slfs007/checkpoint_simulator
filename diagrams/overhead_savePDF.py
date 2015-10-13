26c9c28c9d224d89107c337a1521f380343c3bd2
mkb <slfs009@163.com>
2015-10-13 15:11:06 +0800

    add label in overhead_savePDF.py

--- diagrams/overhead_savePDF.py
+++ diagrams/overhead_savePDF.py
@@ -2,6 +2,7 @@ __author__ = 'mk'
 
 import matplotlib.pyplot as plt
 import sys
+import math
 import numpy as np
 dataDir = sys.argv[1]
 resDir = sys.argv[2]
@@ -19,13 +20,13 @@ for i in range(0,5,1):
     for eachLine in file.readlines():
         xStr,yStr = eachLine.split()
         x.append(int(xStr))
-        y.append(int(yStr))
+        y.append(math.log(float(yStr)))
     file.close()
-    npX = np.array(x)
-    npY = np.array(y)
-    plt.plot(npX,npY,label=algLabel[i],linewidth=2)
+
+    plt.plot(x,y,label=algLabel[i],linewidth=1)
 plt.xlabel("Data Size")
 plt.ylabel("Overhead")
 plt.title("Overhead Per Checkpoint")
+plt.legend()
 plt.savefig(resDir + "OverheadPerCheckpoint.pdf")
 
