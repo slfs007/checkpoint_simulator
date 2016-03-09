7f1177dcb49933466591147b47e2fe8c5ff0570e
Wenbo Lang <slfs009@163.com>
2016-03-09 14:55:12 +0800

    Update latency_test.py

--- diagrams/latency_test.py
+++ diagrams/latency_test.py
@@ -19,27 +19,22 @@ for i in range(0,6,1):
     time=[]
     latency=[]
     timeSum = 0
-    lastTime = 0
-    #0.1 sec
-    scale = 100000000
+
     count = 0
     tick = 0
     for eachLine in logFile.readlines():
         timeNsStr,latencyNsStr = eachLine.split(",")
-        timeNs = int(timeNsStr)
+        
         latencyNs = int(latencyNsStr)
-        count=count+1
-        timeSum += timeNs
-        if (count == int(uf))
+        count = count + 1
+        timeSum = timeNs + latencyNs
+        if (count == int(uf)):
         	time.append(tick)
-        	tick=tick+1
+        	tick = tick + 1
         	latency.append(timeSum)
         	count = 0
         	timeSum = 0
         	
-    baseTime = time[0]
-    for j in range(0,len(time),1):
-        time[j] = time[j] - baseTime
 
     plt.plot(time,latency,label=algLabel[i],linewidth=1)
     logFile.close()
